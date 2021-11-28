458292d014348d1edf849245a0550d46f2695e19
Pedro <pedro.carmo@gprt.ufpe.br>
2021-11-28 15:22:24 -0300

    update main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from objective_functions import Ackley, Griewank, Colville, Trid
+from pymoo.problems.single import ackley
+from objective_functions import Ackley, Griewank, Colville, Trid, KnapSack
 from pymoo.algorithms.soo.nonconvex.es import ES
 from pymoo.algorithms.soo.nonconvex.de import DE
 from pymoo.optimize import minimize
@@ -9,9 +10,9 @@ from pymoo.operators.sampling.lhs import LHS
 from pymoo.factory import get_problem
 
 
-iterations = 30  # 1000  #10000
-problems = [Trid(), Ackley(), Griewank(), Colville()]
-for problem in problems:
+def q1(problem=Ackley()):
+    iterations = 30  # 1000  #10000
+
     print(problem.name)
     es = ES()
     de = DE()
@@ -29,3 +30,28 @@ for problem in problems:
     stat, pvalue = ttest_ind(es_results, de_results)
     print('Estatistica do teste T para estes dois algoritmos:', stat)
     print('P-valor encontrado:', pvalue)
+
+def q2():
+    pass
+
+if __name__ == "__main__":
+    
+    value = input("Digite uma opção (ackley, griewank, colville, trid ou knapsack):\n")
+    print(f'You entered {value}')
+    if value == 'ackley':
+        q1(Ackley())
+    elif value == 'griewank':
+        q1(Griewank())
+    elif value == 'colville':
+        q1(Colville())
+    elif value == 'trid':
+        q1(Trid())
+    elif value == 'knapsack':
+        bags = input("Digite o numero de mochilas:\n")
+        print(f'You entered {bags}')
+        objs = input("Digite o numero de objetos:\n")
+        print(f'You entered {objs}')
+        #q2(KnapSack(bags, objs))
+    else:
+        print("Opção não disponível")
+        exit()
