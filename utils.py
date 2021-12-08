27de6b470328e8d50c2ca7583dec614eac353486
LFBJC <lfbjc@cin.ufpe.br>
2021-12-08 10:38:57 -0300

    teste T retornando resultados e correcao de erro: trata-se de uma minimizacao entao os menores valores sao melhores

--- utils.py
+++ utils.py
@@ -8,8 +8,19 @@ def t_test(names, results):
     print('    P-valor encontrado:', pvalue[0])
     if pvalue < 0.05:
         if np.mean(results[0]) > np.mean(results[1]):
-            print('    O algoritmo ' + names[0] + ' obteve resultados melhores com nível de confiança 95%')
-        else:
             print('    O algoritmo ' + names[1] + ' obteve resultados melhores com nível de confiança 95%')
+            best_name = names[1]
+            best_list = results[1]
+        else:
+            print('    O algoritmo ' + names[0] + ' obteve resultados melhores com nível de confiança 95%')
+            best_name = names[0]
+            best_list = results[0]
     else:
         print('    Para um nível de significância de 5% os resultados são equivalentes')
+        if np.std(results[0]) < np.std(results[1]):
+            best_name = names[0]
+            best_list = results[0]
+        else:
+            best_name = names[1]
+            best_list = results[1]
+    return best_name, best_list
