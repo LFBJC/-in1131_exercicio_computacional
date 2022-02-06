8a12427ec0eaaed157c503353047d419e69229a7
LFBJC <lfbjc@cin.ufpe.br>
2022-02-05 21:22:48 -0300

    definição do objetivo final

--- projeto/main.py
+++ projeto/main.py
@@ -1,11 +1,10 @@
-from knap import KnapSack
-from DE import ourDE
+# from DE import ourDE
 
-items = [(3, 3), (2, 2), (1, 1), (2.2, 2), (1.4, 1), (3.8, 4), (0.2, 1), (0.1, 1), (0.13, 1),
-        (2.8, 3), (1.5, 2), (2, 2), (3.1, 3), (1.2, 1), (1.7, 3), (1.1, 2), (0.3, 1)]
-bags_list = [13, 9, 7]
-
-problem = KnapSack(knapsacks=bags_list, items=items)
-init_pop = len(bags_list) * len(items) * 20
-
-ourDE.solver(problem=problem, init_pop=init_pop)
+print('Digite as dependências das atividades.')
+print('Para fazê-lo digite pares de atividades separados por espaço com o seguinte formato:')
+print('        nome_da_atividade: dependências')
+print('Exemplo: atividade2:atividade1 atividade4:atividade3 atividade5:atividade3,atividade4')
+activities_string = input()
+activities_string_components = activities_string.split()
+graph = [[component.split(':')[0], component.split(':')[1].split(',')] for component in activities_string_components]
+# topologically_sorted_activities = khan(graph)
