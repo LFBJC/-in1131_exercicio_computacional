e933bfbeb93b8c398771cb55e5cbec587fc337ab
LFBJC <lfbjc@cin.ufpe.br>
2022-02-05 21:32:54 -0300

    alteração do print explicativo para considerar o caso de uma atividade não depender de nenhuma

--- projeto/main.py
+++ projeto/main.py
@@ -3,8 +3,8 @@
 print('Digite as dependências das atividades.')
 print('Para fazê-lo digite pares de atividades separados por espaço com o seguinte formato:')
 print('        nome_da_atividade: dependências')
-print('Exemplo: atividade2:atividade1 atividade4:atividade3 atividade5:atividade3,atividade4')
-activities_string = input()
+print('Exemplo: atividade2:atividade1 atividade4:atividade3 atividade5:atividade3,atividade4 atividade6:')
+activities_string = input('Digite aqui: ')
 activities_string_components = activities_string.split()
 graph = [[component.split(':')[0], component.split(':')[1].split(',')] for component in activities_string_components]
 # topologically_sorted_activities = khan(graph)
