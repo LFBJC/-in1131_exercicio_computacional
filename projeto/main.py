3e834681f8b05d743531f3fe4eb87d5d8b0db784
LFBJC <lfbjc@cin.ufpe.br>
2022-02-05 22:42:43 -0300

    algoritmo de khan

--- projeto/main.py
+++ projeto/main.py
@@ -1,4 +1,5 @@
 # from DE import ourDE
+from utils import khan
 
 print('Digite as dependências das atividades.')
 print('Para fazê-lo digite pares de atividades separados por espaço com o seguinte formato:')
@@ -8,6 +9,8 @@ activities_string = input('Digite aqui: ')
 activities_string_components = activities_string.split()
 graph = [[component.split(':')[0], component.split(':')[1].split(',')] for component in activities_string_components]
 dependent_tasks = [restriction[0] for restriction in graph]
-graph += [[e, []] for component in activities_string_components for e in component.split(':')[1].split(',')
-          if e not in dependent_tasks]
-# topologically_sorted_activities = khan(graph)
+all_nodes = list(set([task for c in graph for task in [c[0]]+c[1]]))
+not_dependent = [node for node in all_nodes if node not in dependent_tasks]
+graph += [[task, []] for task in not_dependent]
+topologically_sorted_activities = khan(graph)
+print(topologically_sorted_activities)
--- projeto/utils.py
+++ projeto/utils.py
@@ -4,4 +4,18 @@ def random_key_decoder(x):
 
 
 def khan(graph):
-    pass
+    ret = []
+    no_incoming_edge = [component[0] for component in graph if component[1] == []]
+    while no_incoming_edge != []:
+        current_node, no_incoming_edge = no_incoming_edge[0], no_incoming_edge[1:]
+        ret.append(current_node)
+        for i, component in enumerate(graph):
+            if current_node in component[1]:
+                graph[i][1] = [node for node in component[1] if node != current_node]
+                if graph[i][1] == []:
+                    no_incoming_edge.append(component[0])
+    edges = [(component[0], x) for component in graph if component[1] != [] for x in component]
+    if edges != []:
+        raise ValueError("The graph has cycles")
+    else:
+        return ret
