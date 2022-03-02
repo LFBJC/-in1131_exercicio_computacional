5310a648496f706d611bf5bfc32465e4d26331eb
Pedro <pedro.carmo@gprt.ufpe.br>
2022-03-02 12:25:09 -0300

    removido o csv creator

--- projeto/main.py
+++ projeto/main.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 
 from mutation import *
 
-CRITERION = ("n_gen", 10)
+CRITERION = ("n_gen", 100)
 ITERATIONS = 30
 
 #----------------versão antiga onde é preciso digitar os inputs---------------#
@@ -58,7 +58,7 @@ while True:
     representation = 'RK'
     mutation = 'agressive'
     if os.path.isfile(file_name):
-        for _ in tqdm(range(ITERATIONS)):
+        for itera in tqdm(range(ITERATIONS)):
             graph, times_dict, r_cap_dict, r_cons_dict, r_count, act_pre = problem_from_json(file_name)
             if representation == 'RK':
                 problem = RCPSP_RandomKeyRepresentation(graph=graph, times_dict=times_dict, r_cap_dict=r_cap_dict, r_cons_dict=r_cons_dict, r_count=r_count, act_pre=act_pre)
@@ -68,14 +68,14 @@ while True:
 
             pop= math.exp(3.551 + (22.72/jobs))
             if sampling_tipe == 'standard':
-                de = DE(pop_size=int(pop), variant="DE/best/1/bin", CR=0.5, jitter=True, mutation=get_mutation("perm_inv", prob=0.7)) 
+                de = DE(pop_size=int(pop), variant="DE/best/1/bin", CR=0.7, jitter=True, mutation=get_mutation("perm_inv", prob=0.8)) 
             if sampling_tipe == 'SamplingWithSelection':
                 de = DE(pop_size=int(pop), sampling=SamplingWithSelection())  # DE(sampling=SamplingRespectingPrecedence(pop_ratio=0.8, max_depth=30))            
             if sampling_tipe == 'SamplingRespectingPrecedence':
                 de = DE(pop_size=int(pop), sampling=SamplingRespectingPrecedence(pop_ratio=1, max_depth=30))  # DE(sampling=SamplingRespectingPrecedence(pop_ratio=0.8, max_depth=30))            
             
             res = minimize(problem, de, CRITERION, save_history=True,               
-                           verbose=False)
+                           verbose=True)
 
             if res.F is not None:
                 x_results.append(res.X)
@@ -83,19 +83,18 @@ while True:
             else:
                 none_count += 1
 
-        fitness_results = [f for f in fitness_results if f is not None]
+            fitness_results = [f for f in fitness_results if f is not None]
 
-        new_row = {'instance': instance,  'min_makespan':min(fitness_results), 'average_makespan':np.mean(fitness_results), 
-                     'std_makespan':np.mean(fitness_results), 'sampling_tipe':sampling_tipe, 'representation': representation, 'mutation': mutation}
+            new_row = {'instance': instance,'sampling_tipe':sampling_tipe, 'representation': representation, 'mutation': mutation}
 
-        filename_save = ''.join("{}_{}".format(k, v) for k, v in new_row.items())
-        #print(str(res.history), file=open(str(os.getcwd()) + '/data/solutions/' + filename_save +'.txt', "a"))
-        
-        fig = plt.figure(figsize=(3, 6))
-        val = [e.opt.get("F")[0] for e in res.history]
-        plt.plot(np.arange(len(val)), val)
-        #plt.show()
-        fig.savefig(str(os.getcwd()) + '/data/solutions/j' + str(jobs) + '/' + filename_save + '.png', dpi=fig.dpi)
+            filename_save = ''.join("{}_{}".format(k, v) for k, v in new_row.items())
+            #print(str(res.history), file=open(str(os.getcwd()) + '/data/solutions/' + filename_save +'.txt', "a"))
+            
+            fig = plt.figure(figsize=(3, 6))
+            val = [e.opt.get("F")[0] for e in res.history]
+            plt.plot(np.arange(len(val)), val)
+            #plt.show()
+            fig.savefig(str(os.getcwd()) + '/data/solutions/j' + str(jobs) + '/' + filename_save + '_' + str(itera) + '.png', dpi=fig.dpi)
 
         #np.savetxt(str(os.getcwd()) + '/data/solutions/' + 'j' + str(jobs) + '/' + str(new_row) +'.txt', res.history , delimiter=",")
 
